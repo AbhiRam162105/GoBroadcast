baef1c8c2b63bbb9fba308cd160c425eadd58d73
Abhiram162105 <abhiyanampally@gmail.comgit>
2024-05-16 01:39:35 +0530

    refactor: Restructure websockets code and add connection management
    This commit refactors the websockets code by restructuring the `HandleConnections` function and adding connection management. It introduces a `Connection` struct to store the websocket connection and a `Connections` sync.Map to store all active connections. The `HandleConnections` function now generates a unique ID for each connection, stores it in the global map, and broadcasts the ID to all clients. It also processes and broadcasts incoming messages to all clients

--- internal/websockets.go
+++ internal/websockets.go
@@ -1,9 +1,11 @@
 package websockets
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -16,6 +18,12 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
+type Connection struct {
+	Conn *websocket.Conn
+}
+
+var Connections sync.Map = sync.Map{}
+
 func HandleConnections(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
@@ -24,25 +32,54 @@ func HandleConnections(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
+	// Generate a unique ID for this connection
+	id := generateID()
+
+	// Store the connection in the global map
+	Connections.Store(id, &Connection{Conn: conn})
+
+	// Broadcast the connection ID to all clients so they know who is connected
+	broadcastMessage(id)
+
 	for {
-		messageType, message, err := conn.ReadMessage()
+		_, message, err := conn.ReadMessage()
 		if err != nil {
 			log.Println("read:", err)
 			break
 		}
 
-		// Check if the received message is binary (assuming blobs are sent as binary data)
-		if messageType == websocket.BinaryMessage {
-			fmt.Println("Video received")
-			fmt.Printf("Data received: %x\n", message)
-		}
-
-		// Here you would process the video stream data
-		// For simplicity, we're just echoing back the received data
-		err = conn.WriteMessage(messageType, message)
-		if err != nil {
-			log.Println("write:", err)
-			break
-		}
+		// Process the message and broadcast it to all clients
+		processAndBroadcastMessage(id, message)
 	}
+}
+
+func generateID() string {
+	// Simple ID generation for demonstration purposes
+	return "client-" + strconv.FormatInt(time.Now().UnixNano(), 10)
+}
+
+func broadcastMessage(id string) {
+	// Iterate over all connections and broadcast the message
+	Connections.Range(func(key, value interface{}) bool {
+		c := value.(*Connection)
+		err := c.Conn.WriteMessage(websocket.TextMessage, []byte("New client connected: "+id))
+		if err != nil {
+			log.Println("broadcast:", err)
+		}
+		return true
+	})
+}
+
+func processAndBroadcastMessage(clientID string, message []byte) {
+	// Process the message here
+	// For demonstration, we're just broadcasting the message back to all clients
+
+	Connections.Range(func(key, value interface{}) bool {
+		c := value.(*Connection)
+		err := c.Conn.WriteMessage(websocket.TextMessage, message)
+		if err != nil {
+			log.Println("broadcast:", err)
+		}
+		return true
+	})
 }
